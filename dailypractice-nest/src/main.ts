52c6edff0a301cd960a9b651649090026fd99cb1
Fernando Tadeo <fernandotadeos@gmail.com>
2024-01-15 21:22:16 -0300

    se agrega morgan para utilizar logs de cada request que se hace

--- dailypractice-nest/src/main.ts
+++ dailypractice-nest/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService} from '@nestjs/config'
+import { ConfigService} from '@nestjs/config';
+import * as morgan from 'morgan';
+
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+
+  app.use(morgan('dev'))
+
   const configService = app.get(ConfigService)
   console.log(configService.get('PORT'))
   await app.listen(configService.get('PORT'));
