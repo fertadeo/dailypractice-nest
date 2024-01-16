cc7db613e930d470e548a65c5d798df8feb7ce23
Fernando Tadeo <fernandotadeos@gmail.com>
2024-01-15 22:00:48 -0300

    se configura cors

--- dailypractice-nest/src/main.ts
+++ dailypractice-nest/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService} from '@nestjs/config';
 import * as morgan from 'morgan';
+import { CORS } from './constants';
 
 
 async function bootstrap() {
@@ -11,8 +12,14 @@ async function bootstrap() {
   app.use(morgan('dev'))
 
   const configService = app.get(ConfigService)
-  console.log(configService.get('PORT'))
+
+  app.setGlobalPrefix('api')
+
+  app.enableCors(CORS)
+
   await app.listen(configService.get('PORT'));
+
+  console.log(`Application is running on: ${await app.getUrl()}`)
 }
 bootstrap();
  
